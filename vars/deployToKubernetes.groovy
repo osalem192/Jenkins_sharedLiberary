9b3bb170a5dc0f82e31c6c5f67c0442610477821
osalem192 <osalem192@gmail.com>
2025-07-15 21:45:32 +0300

    fixed brackets

--- vars/deployToKubernetes.groovy
+++ vars/deployToKubernetes.groovy
@@ -1,5 +1,5 @@
 def call(String credentials) {
-    withCredentials([file(credentialsId: $(credentials), variable: 'KUBECONFIG_FILE')]) {
+    withCredentials([file(credentialsId: "${credentials}", variable: 'KUBECONFIG_FILE')]) {
         sh '''
         export KUBECONFIG=$KUBECONFIG_FILE
         kubectl apply -f deployment.yaml
