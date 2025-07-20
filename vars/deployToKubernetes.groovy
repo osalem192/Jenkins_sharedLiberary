ecf4a4d32d1f1bac8f9c1b01e8987b55f53fb43d
osalem192 <osalem192@gmail.com>
2025-07-21 00:08:48 +0300

    edited deployToKubernets to have namespace

--- vars/deployToKubernetes.groovy
+++ vars/deployToKubernetes.groovy
@@ -1,8 +1,8 @@
-def call(String credentials) {
+def call(String credentials, String namespace = "default") {
     withCredentials([file(credentialsId: "${credentials}", variable: 'KUBECONFIG_FILE')]) {
         sh '''
         export KUBECONFIG=$KUBECONFIG_FILE
-        kubectl apply -f deployment.yaml
+        kubectl apply -f deployment.yaml -n ${namespace}
         '''
     }
-} 
+}   
